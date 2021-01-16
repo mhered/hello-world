e51a55235a2c91834da6439ddacd798195711062
Manuel Heredia <75589082+mhered@users.noreply.github.com>
2021-01-16 23:54:13 +0100

    Update of hidden.py
    Public version of hidden.py in GitHub

--- hidden.py
+++ hidden.py
@@ -1,11 +1,5 @@
 # dummy hidden.py file with hidden keys
 # should not be updated to github
-<<<<<<< HEAD
-# the first time it didnt work,
-# but this modification should not be visible in github
-=======
-# this modification should not upload to github
->>>>>>> 24b2230e82934f812c4818b258479c1a38ec09b1
 
 def oauth():
     return {
