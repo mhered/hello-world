8131b627aaef939bfb4cac6522277402441a2977
mhered <manolo.heredia@mail.com>
2021-01-16 23:41:05 +0100

    Added dummy .ignore and hidden.py files

---
+++ hidden.py
@@ -0,0 +1,9 @@
+# dummy hidden.py file with hidden keys
+# should not be updated to github
+
+def oauth():
+    return {
+        "consumer_key": "XX...XX",
+        "consumer_secret": "XX...XX",
+        "token_key": "XX...XX",
+        "token_secret": "XX...XX"}
